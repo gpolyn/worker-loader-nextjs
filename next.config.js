dabd560f6987767aee3892cc5a0b6b592133f602
Gallagher Polyn <gallagher.polyn@gmail.com>
2018-05-19 13:04:57 -0400

    writing worker to dist

--- next.config.js
+++ next.config.js
@@ -4,10 +4,20 @@ module.exports = withSourceMaps({
   webpack(cfg, {isServer}){
 
     if (!isServer){
-      cfg.module.rules.push({
-        test: /\.worker\.js$/,
-        use: ['worker-loader', 'babel-loader']
-      })
+      cfg.module.rules.push(
+        {
+          test: /\.worker\.js$/,
+          use: ['worker-loader', 'babel-loader']
+          // use: [{loader: 'worker-loader', options:{publicPath:'/workers'}}, 'babel-loader']
+        },
+				{
+					test: /\.worker\.js$/,
+					loader: 'emit-file-loader',
+					options: {
+						name: 'dist/[path][name].[ext]'
+					}
+				},
+      )
     }
 
     return cfg
